e986c32b2118e6e4c3636711c8144c50525db9ca
Colin <colin.diesh@gmail.com>
2017-07-13 01:31:16 -0400

    Add build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 node dojo/dojo.js load=build --require "app/init.js" --profile "app/app.profile.js" --releaseDir "dist";
 cp index.html dist
+mkdir dist/app
+cp app/init.js dist/app/
